d32e759059f9f953f0c780b7adacc46026763adc
Mkamboykos <mmeri032@fiu.edu>
2021-10-06 22:04:03 -0400

    template for API with postman

--- server/Models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = mongoose.Schema("Post",{
-	title: String,
-	content: String,
-})
-
-module.exports = mongoose.model("Post", schema)
--- server/Routes/LoginRoute.js
+++ server/Routes/LoginRoute.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../Models/Users");
 
-router.get('/User', async (req, res) =>{
-	res.send(posts)
+router.post('/UserLogin', async (req, res) =>{ 
+	const newUser = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	newUser
+		.save()
+		.then(result => {
+			console.log(result);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
+
+
+router.get('/checkIfExistUser', async (req, res) =>{ 
+	const {username, password} = req.body;
+
+	const checkUser = await User.find({username:username});
+	const checkPassword = await User.find({password:password})
+
+	try {
+		// res.send(checkUser, checkPassword).body;
+		res.status(200).send(checkUser +  checkPassword)
+	  } catch (error) {
+		response.status(500).send(error);
+	  }
 });
 
 module.exports = router;
