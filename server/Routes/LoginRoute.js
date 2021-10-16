e1cebe4b34dc6c6cd9a8b85c65efa4320b378bc0
hjanday <jandaypreet@gmail.com>
2021-10-15 23:30:54 -0400

    Added Routes for Login Validation

--- server/Routes/LoginRoute.js
+++ server/Routes/LoginRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../Models/Users");
-
+const {Users} = require("../Models/Users");
+const bcrypt = require('bcrypt');
 router.post('/UserLogin', async (req, res) =>{ 
 	const newUser = new User({
 		username: req.body.username,
@@ -34,3 +34,27 @@ router.get('/checkIfExistUser', async (req, res) =>{
 });
 
 module.exports = router;
+
+router.post('/Auth/Login', async (request, response) =>{
+    // Define User Object from Models
+    const {username, password} = request.body;
+    //Validate User Schema Username and Password
+    const user = await Users.findOne({username:username})
+    const user_pass = await Users.findOne({password:password})
+    //Define salts for password and hashed password
+    
+    const password_salt = await bcrypt.genSalt(10)
+    const hashed_password = await bcrypt.hashPassword(password, password_salt)
+    
+    try{
+        if(user && hashed_password == user_pass) {
+        response.status(200).send(response.json())
+        }
+    } 
+        catch(error){
+            response.status(422).send("Wrong username or password combination failed")
+        }
+    
+    
+    
+    });
