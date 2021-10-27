04cb026a764ac33a6208a8cf0c69854a41d2cea0
PenguinAmbush <jller023@fiu.edu>
2021-10-27 17:04:00 -0400

    changed signup.js

--- server/Models/Users.js
+++ server/Models/Users.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose")
 
 const UsersSchema = mongoose.Schema({
+	email: {
+		type: String,
+		required: [true, 'Email']
+	},
 	username: {
 		type: String,
 		required: [true, 'User']
+	},
+	password: {
+		type: String,
+		required: [true, 'Password']
+	},
+	Gender: {
+		type: String,
+		required: [true, 'Gender']
+	},
+	Age: {
+		type: Number,
+		required: [true, 'Age']
+	},
+	City: {
+		type: String,
+		required: [true, 'City']
+	},
+	State: {
+		type: String,
+		required: [true, 'City']
 	}
-		,
-	password: String
 })
 
 module.exports = mongoose.model("Users", UsersSchema);
--- server/Routes/SignUpRoute.js
+++ server/Routes/SignUpRoute.js
@@ -2,7 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/basicInfo', async (req, res) =>{
-    
+  const newUser = new User({
+        email:    req.body.email,
+		username: req.body.username,
+		password: req.body.password,
+        Gender:  req.body.Gender,
+        Age:      req.body.Age,
+        City:     req.body.City,
+        State:    req.body.State
+	});
+
+	newUser
+		.save()
+		.then(result => {
+			console.log(result);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 module.exports = router;
