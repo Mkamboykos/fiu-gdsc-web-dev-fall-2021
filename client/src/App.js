bb940e5835d71309d770f6762d3515d29fd4500f
maneywu <mwu019@fiu.edu>
2021-11-03 19:44:17 -0400

    Added Nav and About

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@material/react-icon-button/dist/icon-button.css';
-import {Route} from 'react-router-dom'
-import Home from './components/Home'
-import Dashboard from './components/Dashboard'
-import RecipeCard from './components/RecipeCard'
-
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@material/react-icon-button/dist/icon-button.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import RecipeCard from "./components/RecipeCard";
+import About from "./components/About";
+import Nav from "./components/Nav";
+ 
 function App() {
   return (
-    <div className="App">
-
-       {/* The Router is used to create different links for each class */}
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/Dashboard" component={Dashboard}/>
-
-
-      <Route exact path="/test" component={RecipeCard}/>
-
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          {/* The Router is used to create different links for each class */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Dashboard" component={Dashboard} />
+          <Route exact path="/About" component={About} />
+ 
+          <Route exact path="/test" component={RecipeCard} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
+ 
 export default App;
+ 
