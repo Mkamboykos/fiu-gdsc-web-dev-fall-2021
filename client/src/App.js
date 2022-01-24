9871454e2c341fbf663d601d761d7c0760abb0a0
camas002 <camas002@fiu.edu>
2022-01-24 01:04:58 -0500

    Fixed home not loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./index.css";
 
 import React, {lazy, Suspense} from 'react'
-import { BrowserRouter as Route, Link, Routes, Router} from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom'
 
 const Home = lazy(() => import('./components/Home'))
 const Dashboard = lazy(() => import('./components/Dashboard'))
@@ -12,12 +12,6 @@ function App() {
 
   return (
   <Suspense fallback={"LOADING"}>
-    <Router>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="about"> About </Link>
-      </nav>
-    </Router>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/Dashboard" element={<Dashboard/>}/>
