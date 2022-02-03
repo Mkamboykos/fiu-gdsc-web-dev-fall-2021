7230cd27c2b1e886f3db80e1f79a886d5f5c5c49
blusubz <sk8ermorris@hotmail.com>
2022-02-03 00:50:10 -0500

    started making button, have to customize more, testing my first actual commit/push ever

--- client/src/components/Forgot.js
+++ client/src/components/Forgot.js
@@ -1,20 +1,44 @@
 import React from 'react';
+// import styled from 'styled-components';
+
+// Set up forgot password page that takes in an email input after clicking 
+// a "continue" button. For now, only do the label and button.
+
+// TODO: 
+/*
+    1. Create label for email
+    2. add continue button under email label 
+    3. add a small description thats says "please type in your password"
+    4. (optional) validate input for email
+*/ 
+
+// const Button = style.button`
+//   background-color: blue;
+//   color: white;
+//   padding: 5px 15px;
+//   border-radius: 5px;
+//   outline: 0;
+//   text-transform: uppercase;
+//   cursor: pointer;
+//   box-shadow: 0px 2px lightgray;
+//   &:hover {
+//     background-color: #283593;
+//   }
+//   `
+
+
+function clickMe(){
+  alert('You clicked me')
+}
 
 function Forgot(){
 
 
-
-
-
-
-
     return (
       <div>
-        
-  
-  
-  
-      
+        <button onClick={clickMe}>
+          Button
+        </button>
       </div>
     )
   }
---
+++ server/Routes/ForgotRoute.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const forget = require('../Models/Users');
+const nodemailer = require ('nodemailer'); 
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const {body, validationResult} = require('express-validator');
+
+
+router.post ('/Email',async (req, res) => {
+    const {email} = req.body;
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return next(new ValidationException(errors.array()));
+    }
+
+    const checkEmail = await forget.findOne({ where: {email: email}});
+
+    if (email === checkEmail.email)
+    {
+        try 
+		{
+            //Reusable transporter object
+            let transporter = nodemailer.createTransport({
+                host: process.env.MAIL_HOST,
+                port: process.env.MAIL_PORT,
+                secure: false,
+                auth: {
+                    user: process.env.MAIL_USER,    //Unique user found in env
+                    pass: process.env.MAIL_PASS,    //Unique password found in env
+                },
+            });
+
+            //Verify that the transporter connection is established
+            transporter.verify(function(error, success) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Server is ready to take our messages');
+                }
+            });
+
+            var num = Math.floor( Math.random() * 10000);    //Random code of 4 whole digits
+            code = num;                                       //Pass num into 'code: code'
+
+            //Create message that will be sent to user email
+            const msg = {
+                from: `"GDSC Team" <${process.env.MAIL_FROM}>`, // sender address
+                to: email, // list of receivers
+                subject: "Hello",
+                text: "This is your code: " + code,
+                html: `<b>Hey there!</b> <br/>Here is your code ${code}`, 
+            }
+
+            const info = await transporter.sendMail(msg);       //Verify that message gets sent
+
+            console.log("Message sent: %s", info.messageId);    //Display mail Id
+            //console.log ("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+            res.json ({code: code});
+        }
+        catch (err){
+            console.log (err);
+        }
+    }
+    else
+    {
+        res.status(400).send({error:'Email could not be found'});
+    }
+});
+
+module.exports = router;
