03cd9751f37d1bbbdffb138bb8dc80a6bf1b37d0
camas002 <camas002@fiu.edu>
2022-02-28 19:38:52 -0500

    About Nav Bar

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 
 function About(){
 
@@ -10,6 +11,10 @@ function About(){
 
     return (
         <div>
+            <nav>
+                <Link to={`/`}>Home Icon</Link>
+                <Link to={`/`} style={{textDecoration: "none"}}><button>Login</button></Link>
+            </nav>
             <h2>About</h2>
 
 
