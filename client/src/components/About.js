70037cb1fb6a3127df0998cb591c182fce188f8e
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2022-03-08 23:54:24 -0500

    about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,7 +6,45 @@ import logo from '../Images/logo.svg'
 function About(){
 
 
+    const AboutContainer = () =>{
+        return (
+          <div className='aboutPageContainer'>
+            <div className='aboutRowContainer'>
+              
+                <div className='aboutLeftPanel'>
 
+                </div>
+
+                <div className='aboutRightPanel'>
+                    <div className='aboutRightTextContainer'>
+                        <div className='aboutRightTitle'>
+                            <h1 className='textBold'>
+                                Who We Are
+                            </h1>
+                        </div>
+                        <div className='aboutRightText'>
+                            <h5 className='font-inherit'>
+                            We are the Google Developer Student Club Web Development Team at Florida International University!
+                            <br/>
+                            <br/>
+                            Together we are a group of individuals that are passionate about web technologies and decided to experiment with a real world application.
+                            <br/>
+                            <br/>
+                            College Budgeter is a passion project that 
+                            is built for college students by college students. 
+
+
+                            </h5>
+                        </div>
+                        
+                        
+                    </div>
+                    
+                </div>
+            </div>
+          </div>
+        )
+      }
 
 
 
@@ -25,6 +63,9 @@ function About(){
                     </div>
                 </div>
             </nav>
+            <div className="centerItems">
+                {AboutContainer()}
+            </div>
             <img src={homeBackground} className='homeBackground' alt="" loading="lazy"/>
         </div>
     )
