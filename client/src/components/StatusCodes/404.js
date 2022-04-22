2e416eb34b20e121c9625bd20f18d2a03c4320f3
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2022-04-22 08:47:28 -0400

    customized 404 page

--- client/src/components/StatusCodes/404.js
+++ client/src/components/StatusCodes/404.js
@@ -3,9 +3,11 @@ import {Link} from 'react-router-dom'
 
 function NotFound() {
   return(
-      <div>
-          <h1>404 - Page Not Found</h1>
-          <Link to="/" className="link">HOME</Link>
+      <div className='pageNotFoundContainer'>
+          <h1 className='pageNotFound404'>404</h1>
+          <h1> Page Not Found</h1>
+          <br/>
+          <span>Return back <Link to="/" className="link">Home</Link></span>
       </div>
   )
 }
